e976a4b868fd75f887831265568f10e2767b1ee6
maxwell-x <halfquake@gmail.com>
2023-02-02 00:09:42 +0200

    some example code to think about

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -3,10 +3,11 @@ import Navbar from "./navbar";
 
 export default function Layout({ children }: any) {
   return (
-    <>
+    <div className="min-h-full">
       <Navbar />
-      <main>{children}</main>
-      {/* <Footer /> */}
-    </>
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{children}</div>
+      </main>
+    </div>
   );
 }
