a43dcfb1da3b8936e23fa2276958e1055186a988
Igor Mariutsa <igor.mariuta@gmail.com>
2023-03-17 12:13:31 +0200

    removing layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,9 @@
-import Layout from "@/layout/Layout";
 import { AppProps } from "next/app";
 
 import "../assets/styles/styles.scss";
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default App;
