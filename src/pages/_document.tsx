5f26a4d96735a8d00c878b90e5199e91c4e632fa
Igor Mariutsa <igor.mariuta@gmail.com>
2023-03-17 11:26:50 +0200

    fixing head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,10 @@
-import { Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      {/* <Head />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <Head />
+      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;500;600;700;800&display=swap"
