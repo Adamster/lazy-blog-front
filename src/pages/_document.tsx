e4e8f28bfe88d18e6f89bbcd05d0bc50e096fabe
Igor Mariutsa <igor.mariuta@gmail.com>
2023-03-17 12:03:47 +0200

    remove more

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;500;600;700;800&display=swap"
-        rel="stylesheet"
-      /> */}
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,46 +4,6 @@ interface IProps {
   initialData: IPost[];
 }
 
-// const apiUrl = process.env.API_URL;
-const apiUrl = process.env.NEXT_PUBLIC_API;
-
-// async function fetchData<Data>(url: string): Promise<Data> {
-//   const response = await fetch(url);
-//   const data = await response.json();
-//   return data;
-// }
-
-// const fetcher = (url: string): Promise<IPost[]> =>
-//   fetch(url).then((res) => res.json());
-
-// export async function getServerSideProps(): Promise<{ props: IProps }> {
-//   const initialData = await fetcher(`${apiUrl}/posts`);
-//   return { props: { initialData } };
-// }
-
-// Component
-
 export default function Home({ initialData }: IProps) {
-  // const { data, error } = useSWR<Data>(`${apiUrl}/posts`, fetchData);
-
-  // const { data, error } = useSWR<Data>(`${apiUrl}/posts`, fetcher, {
-  //   fallbackData: initialData,
-  // });
-
-  // useEffect(() => {
-  //   console.log(data);
-  // }, []);
-
-  // if (error) return <div>Error</div>;
-  // if (!data) return <div>Loading...</div>;
-
-  return (
-    <>
-      home page
-      {/* {initialData &&
-        initialData.map((post: IPost) => (
-          <PostPreview key={post.id} post={post}></PostPreview>
-        ))} */}
-    </>
-  );
+  return <>home page</>;
 }
