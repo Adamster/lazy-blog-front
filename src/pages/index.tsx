a9c59640c9537d1633eea86ad606285516fd61eb
Igor Mariutsa <igor.mariuta@gmail.com>
2023-03-22 18:52:47 +0200

    дай мне кацхе

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,21 @@ export default function Home({ fallbackData }: IProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps<IProps> = async () => {
+export const getServerSideProps: GetServerSideProps<IProps> = async ({
+  res,
+}) => {
+  // This value is considered fresh for ten seconds (s-maxage=10).
+  // If a request is repeated within the next 10 seconds, the previously
+  // cached value will still be fresh. If the request is repeated before 59 seconds,
+  // the cached value will be stale but still render (stale-while-revalidate=59).
+  //
+  // In the background, a revalidation request will be made to populate the cache
+  // with a fresh value. If you refresh the page, you will see the new value.
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59"
+  );
+
   const fallbackData = await getPosts();
   return { props: { fallbackData } };
 };
