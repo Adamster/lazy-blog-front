3ee5bdd05357ff8e24fa2af5397ef6bfb29fcbbf
Igor Mariutsa <igor.mariuta@gmail.com>
2023-03-22 03:02:30 +0200

    ok than

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -60,6 +60,13 @@ export const authOptions: NextAuthOptions = {
       session.token = token;
       return session;
     },
+    // async redirect({ url, baseUrl }) {
+    //   // Allows relative callback URLs
+    //   if (url.startsWith("/")) return `${baseUrl}${url}`;
+    //   // Allows callback URLs on the same origin
+    //   else if (new URL(url).origin === baseUrl) return url;
+    //   return baseUrl;
+    // },
   },
   pages: {
     signIn: "/auth",
