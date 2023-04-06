5f55486e07459be49e67f2b1356c48e92053278b
Igor Mariutsa <igor.mariuta@gmail.com>
2023-04-06 10:27:03 +0300

    даешь аналитику!!

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -11,9 +12,22 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
+
       <body>
         <Main />
         <NextScript />
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MJC16ETF2H"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'G-MJC16ETF2H');
+          `}
+        </Script>
       </body>
     </Html>
   );
