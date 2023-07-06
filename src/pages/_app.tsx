cc5f3a832a3199ff9db556726f54a6c84ea888c1
Igor Mariutsa <igor.mariuta@gmail.com>
2023-07-06 21:10:31 +0300

    #UI font fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import { Toaster } from "react-hot-toast";
 import "../assets/styles/styles.scss";
 
 const montserrat = Montserrat({
-  weight: ["300", "400", "600", "700"],
+  weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
