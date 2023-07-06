d791cbce031123c685b27a9fd4cff84fbc07e1ef
Igor Mariutsa <igor.mariuta@gmail.com>
2023-07-06 20:45:30 +0300

    #UI font fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,22 +3,23 @@ import { Layout } from "@/layout/Layout";
 import { Analytics } from "@vercel/analytics/react";
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
+import { Montserrat } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 
 import "../assets/styles/styles.scss";
 
-// const montserrat = Montserrat({
-//   weight: ["300", "400", "600", "700"],
-//   subsets: ["latin"],
-// });
+const montserrat = Montserrat({
+  weight: ["300", "400", "600", "700"],
+  subsets: ["latin"],
+});
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
     <>
       <SessionProvider session={session}>
         <ThemeProvider>
-          <Layout>
-            {/* <Layout className={montserrat.className}> */}
+          {/* <Layout> */}
+          <Layout className={montserrat.className}>
             <Component {...pageProps} />
           </Layout>
         </ThemeProvider>
