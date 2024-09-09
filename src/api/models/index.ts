95dd3e4d2eed514d16c48d46622e3608a7eee215
Igor Mariuta <igor.mariuta@gmail.com>
2024-09-09 22:22:59 +0300

    feat: api generate

---
+++ src/api/models/index.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AddCommentRequest';
+export * from './CommentResponse';
+export * from './CommentResponseListResult';
+export * from './CreatePostRequest';
+export * from './LoginRequest';
+export * from './ModelError';
+export * from './PostDetailedResponse';
+export * from './PostDetailedResponseResult';
+export * from './PostResponse';
+export * from './PostResponseResult';
+export * from './ProblemDetails';
+export * from './PublishedPostResponse';
+export * from './PublishedPostResponseListResult';
+export * from './RefreshTokenRequest';
+export * from './RegisterUserRequest';
+export * from './TagResponse';
+export * from './TagResponseListResult';
+export * from './UpdateCommentRequest';
+export * from './UpdatePostRequest';
+export * from './UpdateUserRequest';
+export * from './UserPostItem';
+export * from './UserPostResponse';
+export * from './UserPostResponseResult';
+export * from './UserResponse';
+export * from './VoteDirection';
