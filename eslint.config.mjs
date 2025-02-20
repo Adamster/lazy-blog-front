ecbf178360156d4d783c91191f29c2de9ed620ec
Igor Mariuta <igor.mariuta@gmail.com>
2025-02-20 12:40:34 +0200

    fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,15 +10,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off", // Отключает проверку `any`
+      "@typescript-eslint/no-unused-vars": "off", // Отключает проверку неиспользуемых переменных
+      "no-unused-vars": "off", // Отключает стандартный ESLint
+      "@typescript-eslint/no-empty-interface": "off", // Отключает ошибки на пустые интерфейсы
+      "no-var": "off", // Отключает запрет `var`
+      "@next/next/no-img-element": "off", // Отключает предупреждение про `<img>`
     },
   },
 ];
-
-export default eslintConfig;
