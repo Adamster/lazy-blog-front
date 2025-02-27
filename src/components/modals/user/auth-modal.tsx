11b1a22cb27d0ed469fcf682a688d3fa4a68e28e
Igor Mariuta <igor.mariuta@gmail.com>
2025-02-27 15:34:17 +0200

    fix: modal fix attempt 2

--- src/components/modals/user/auth-modal.tsx
+++ src/components/modals/user/auth-modal.tsx
@@ -38,11 +38,11 @@ export function AuthModal({
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader className="px-10">
+            <ModalHeader className="md:px-10">
               <p>{isLogin ? "Log In" : "Create an Account"}</p>
             </ModalHeader>
 
-            <ModalBody className="px-10">
+            <ModalBody className="md:px-10">
               {isLogin ? (
                 <LoginForm onSuccess={onClose} />
               ) : (
@@ -50,7 +50,7 @@ export function AuthModal({
               )}
             </ModalBody>
 
-            <ModalFooter className="px-10">
+            <ModalFooter className="md:px-10">
               <Button
                 size="md"
                 variant="light"
