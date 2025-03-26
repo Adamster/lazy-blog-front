f1e9c4e6523585b52ceaccaac04af72ebceb5956
Igor Mariuta <igor.mariuta@gmail.com>
2025-03-26 22:34:56 +0200

    fix: test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import { Divider } from "@heroui/react";
 import { PostsList } from "@/features/posts/ui/posts-list";
 import { useAllPosts } from "@/features/posts/model/use-all-posts";
 import { TagsList } from "@/features/tags/ui/tags-list";
+import Head from "next/head";
+import { GenerateMeta } from "@/shared/lib/head/meta-data";
 
 export default function Home() {
   const query = useAllPosts();
@@ -12,26 +14,34 @@ export default function Home() {
   if (query.error) return <ErrorMessage error={query.error} />;
 
   return (
-    <div className="layout-page">
-      <div className="layout-page-content">
-        <PostsList query={query} posts={query.data?.pages?.flat() || []} />
-      </div>
+    <>
+      <GenerateMeta />
 
-      <div className="layout-page-aside">
-        <Divider className="layout-page-divider" orientation="vertical" />
+      <Head>
+        <title>Home</title>
+      </Head>
 
-        <div className="layout-page-aside-content">
-          <aside className="layout-page-aside-content-sticky">
-            <div className="overflow-auto max-w-full">
-              <div className="flex md:flex-col mb-1 md:mb-0 whitespace-nowrap gap-1">
-                <TagsList />
-              </div>
-            </div>
-          </aside>
+      <div className="layout-page">
+        <div className="layout-page-content">
+          <PostsList query={query} posts={query.data?.pages?.flat() || []} />
         </div>
 
-        <Divider className="layout-page-divider-mobile" />
+        <div className="layout-page-aside">
+          <Divider className="layout-page-divider" orientation="vertical" />
+
+          <div className="layout-page-aside-content">
+            <aside className="layout-page-aside-content-sticky">
+              <div className="overflow-auto max-w-full">
+                <div className="flex md:flex-col mb-1 md:mb-0 whitespace-nowrap gap-1">
+                  <TagsList />
+                </div>
+              </div>
+            </aside>
+          </div>
+
+          <Divider className="layout-page-divider-mobile" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
